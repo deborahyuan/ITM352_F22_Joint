95090fab19f816dd9e8d486c5011c342e2573b92
deborahyuan <112019910+deborahyuan@users.noreply.github.com>
2022-11-12 01:39:36 -1000

    New updates

--- Yuan_Deborah_Diep_Evon_Assignment2/server.js
+++ Yuan_Deborah_Diep_Evon_Assignment2/server.js
@@ -14,6 +14,17 @@ app.use(express.static(__dirname + "/public")); // route all other GET/POST requ
 app.use("/css", express.static(__dirname + "/public")); // ?
 app.use(express.urlencoded({ extended: true }));
 
+var fs = require("fs");
+var fname = "user_registration_info.json";
+
+if (fs.existsSync(fname)) {
+	var data = fs.readFileSync(fname, "utf-8");
+	var users = JSON.parse(data);
+	console.log(users);
+} else {
+	console.log("Sorry file " + fname + " does not exist.");
+}
+
 /* functions */
 
 // isNonNegativeInteger tests the input for errors, then returns error messages if any
@@ -51,92 +62,7 @@ app.all("*", function (request, response, next) {
 	next();
 });
 
-// process purchase request (validate quantities, check quantity available)
-
-app.post("/purchase", function (request, response) {
-	// process form by redirecting to the receipt page
-
-	let validinput = true; // assume that all terms are valid
-	let ordered = "";
-	let allblank = false; // assume that it ISN'T all blank
-	let instock = true;
-	let othererrors = false; //assume that there aren't other errors
-
-	let customerquantities = request.body[`quantitytextbox`];
-
-	for (let i in customerquantities) {
-		// Iterate over all text boxes in the form.
-		qtys = customerquantities[i];
-		let model = products[i]["name"];
-		if (qtys == 0) {
-			// assigning no value to certain models to avoid errors in invoice.html
-			ordered += model + "=" + qtys + "&";
-		} else if (
-			isNonNegativeInteger(qtys) &&
-			Number(qtys) <= products[i].quantity_available
-		) {
-			// if qtys is true, added to ordered string
-			products[i].quantity_available -= Number(qtys); // Stock, or quantity_available is subtracted by the order quantity
-			products[i].quantity_sold =
-				Number(products[i].quantity_sold) + Number(qtys); // EC IR1: Total amount sold, or quantity_sold increases by the order quantity
-			ordered += model + "=" + qtys + "&"; // appears in invoice.html's URL
-		} else if (isNonNegativeInteger(qtys) != true) {
-			// quantity is "Not a Number, Negative Value, or not an Integer"
-			validinput = false;
-		} else if (Number(qtys) >= products[i].quantity_available) {
-			// Existing stock is less than desired quantity
-			instock = false;
-		} else {
-			// textbox has gone missing? or some other error
-			othererrors = true;
-		}
-	}
-
-	if (customerquantities.join("") == 0) {
-		// if the array customerquantities adds up to 0, that means there are no quantities typed in
-		allblank = true;
-	}
-
-	// If we found an error, redirect back to the order page, if not, proceed to invoice
-
-	if (allblank) {
-		// if all boxes are blank, there is an error, pops up alert
-		response.redirect(
-			"products_display.html?error=Invalid%20Quantity,%20No%20Quantities%20Selected!%20Please%20type%20in%20values!"
-		);
-	} else if (!validinput) {
-		// quantity is "Not a Number, Negative Value, or not an Integer", pops up alert
-		response.redirect(
-			"products_display.html?error=Invalid%20Quantity,%20Please%20Fix%20the%20Errors%20in%20Red%20on%20the%20Order%20Page!"
-		);
-	} else if (!instock) {
-		// Existing stock is less than desired quantity, pops up alert
-		response.redirect(
-			"products_display.html?error=Invalid%20Quantity,%20Requested%20Quantity%20Exceeds%20Stock"
-		);
-	} else if (othererrors) {
-		// textbox has gone missing? or some other error, pops up alert
-		response.redirect(
-			"products_display.html?error=Invalid%20Quantity,%20Unknown%20Error%20has%20occured"
-		);
-	} else {
-		// If everything is good, redirect to the invoice page.
-		response.redirect("invoice.html?" + ordered);
-	}
-});
-
 // THIS IS FOR LOGIN AND REGISTER
-
-var fs = require("fs");
-var fname = "user_registration_info.json";
-
-if (fs.existsSync(fname)) {
-	var data = fs.readFileSync(fname, "utf-8");
-	var users = JSON.parse(data);
-	console.log(users);
-} else {
-	console.log("Sorry file " + fname + " does not exist.");
-}
 
 /*app.get("/login", function (request, response) {
 	// Give a simple login form
@@ -152,20 +78,7 @@ if (fs.existsSync(fname)) {
 	response.send(str);
 });*/
 
-app.post("/login", function (request, response) {
-	// Process login form POST and redirect to logged in page if ok, back to login page if not
-	let inputusername = request.body["username"].toLowerCase();
-	let inputpassword = request.body["password"];
-	if (typeof users[inputusername] != "undefined") {
-		if (users[inputusername].password == inputpassword) {
-			response.send(`User ${inputusername} is logged in`);
-		} else {
-			response.send(`Wrong password!`);
-		}
-		return;
-	}
-	response.send(`${inputusername} does not exist`);
-});
+loggedin = false;
 
 app.get("/register", function (request, response) {
 	// Give a simple register form
@@ -205,6 +118,116 @@ app.post("/register", function (request, response) {
 	} else {
 		response.send("User " + user_name + " already exists!");
 	}
+});
+
+// process purchase request (validate quantities, check quantity available)
+
+let validinput = true; // assume that all terms are valid
+let ordered = "";
+let allblank = false; // assume that it ISN'T all blank
+let instock = true;
+let othererrors = false; //assume that there aren't other errors
+
+app.post("/purchase", function (request, response) {
+	// process form by redirecting to the receipt page
+
+	let customerquantities = request.body[`quantitytextbox`];
+	for (let i in customerquantities) {
+		// Iterate over all text boxes in the form.
+		qtys = customerquantities[i];
+		let model = products[i]["name"];
+		if (qtys == 0) {
+			// assigning no value to certain models to avoid errors in invoice.html
+			ordered += model + "=" + qtys + "&";
+		} else if (
+			isNonNegativeInteger(qtys) &&
+			Number(qtys) <= products[i].quantity_available
+		) {
+			// if qtys is true, added to ordered string
+			products[i].quantity_available -= Number(qtys); // Stock, or quantity_available is subtracted by the order quantity
+			products[i].quantity_sold =
+				Number(products[i].quantity_sold) + Number(qtys); // EC IR1: Total amount sold, or quantity_sold increases by the order quantity
+			ordered += model + "=" + qtys + "&"; // appears in invoice.html's URL
+		} else if (isNonNegativeInteger(qtys) != true) {
+			// quantity is "Not a Number, Negative Value, or not an Integer"
+			validinput = false;
+		} else if (Number(qtys) >= products[i].quantity_available) {
+			// Existing stock is less than desired quantity
+			instock = false;
+		} else {
+			// textbox has gone missing? or some other error
+			othererrors = true;
+		}
+	}
+
+	if (customerquantities.join("") == 0) {
+		// if the array customerquantities adds up to 0, that means there are no quantities typed in
+		allblank = true;
+	}
+
+	// If we found an error, redirect back to the order page, if not, proceed to invoice
+	if (loggedin == true) {
+		if (allblank) {
+			// if all boxes are blank, there is an error, pops up alert
+			response.redirect(
+				"products_display.html?error=Invalid%20Quantity,%20No%20Quantities%20Selected!%20Please%20type%20in%20values!"
+			);
+		} else if (!validinput) {
+			// quantity is "Not a Number, Negative Value, or not an Integer", pops up alert
+			response.redirect(
+				"products_display.html?error=Invalid%20Quantity,%20Please%20Fix%20the%20Errors%20in%20Red%20on%20the%20Order%20Page!"
+			);
+		} else if (!instock) {
+			// Existing stock is less than desired quantity, pops up alert
+			response.redirect(
+				"products_display.html?error=Invalid%20Quantity,%20Requested%20Quantity%20Exceeds%20Stock"
+			);
+		} else if (othererrors) {
+			// textbox has gone missing? or some other error, pops up alert
+			response.redirect(
+				"products_display.html?error=Invalid%20Quantity,%20Unknown%20Error%20has%20occured"
+			);
+		} else {
+			// If everything is good, redirect to the invoice page.
+			response.redirect("invoice.html?" + ordered);
+		}
+	} else {
+		response.redirect("login.html?" + ordered);
+	}
+});
+
+app.post("/login", function (request, response) {
+	// Process login form POST and redirect to logged in page if ok, back to login page if not
+	let inputusername = request.body[`username`].toLowerCase();
+	console.log(inputusername);
+	let inputpassword = request.body[`password`];
+	if (typeof users[inputusername] != "undefined") {
+		if (users[inputusername].password == inputpassword) {
+			loggedin = true;
+			response.redirect(
+				"loginsuccess.html?success=User%20" +
+					inputusername +
+					"%20is%20logged%20in" +
+					ordered
+			);
+		} else {
+			response.redirect("login.html?error=Password%20is%20incorrect!");
+		}
+		return;
+	}
+	response.redirect("login.html?error=Username%20Does%20Not%20Exist");
+});
+
+app.post("/checkstatus", function (request, response) {
+	if (loggedin == true) {
+		response.redirect("account.html");
+	} else {
+		response.redirect("login.html");
+	}
+});
+app.post("/logout", function (request, response) {
+	loggedin = false;
+	response.send("user logged out");
 });
 // start server
 app.listen(8080, () => console.log(`listening on port 8080`));
