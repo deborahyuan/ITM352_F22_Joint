78d1d5dbbb5a67cfbc58bf43d17e806f1e5bd6bf
deborahyuan <112019910+deborahyuan@users.noreply.github.com>
2022-11-11 20:04:09 -1000

    SLKDfjklwef

--- Yuan_Deborah_Diep_Evon_Assignment2/server.js
+++ Yuan_Deborah_Diep_Evon_Assignment2/server.js
@@ -125,5 +125,86 @@ app.post("/purchase", function (request, response) {
 	}
 });
 
+// THIS IS FOR LOGIN AND REGISTER
+
+var fs = require("fs");
+var fname = "user_registration_info.json";
+
+if (fs.existsSync(fname)) {
+	var data = fs.readFileSync(fname, "utf-8");
+	var users = JSON.parse(data);
+	console.log(users);
+} else {
+	console.log("Sorry file " + fname + " does not exist.");
+}
+
+/*app.get("/login", function (request, response) {
+	// Give a simple login form
+	str = `
+<body>
+<form action="" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+	response.send(str);
+});*/
+
+app.post("/login", function (request, response) {
+	// Process login form POST and redirect to logged in page if ok, back to login page if not
+	let inputusername = request.body["username"].toLowerCase();
+	let inputpassword = request.body["password"];
+	if (typeof users[inputusername] != "undefined") {
+		if (users[inputusername].password == inputpassword) {
+			response.send(`User ${inputusername} is logged in`);
+		} else {
+			response.send(`Wrong password!`);
+		}
+		return;
+	}
+	response.send(`${inputusername} does not exist`);
+});
+
+app.get("/register", function (request, response) {
+	// Give a simple register form
+	str = `
+<body>
+<form action="" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+<input type="email" name="email" size="40" placeholder="enter email"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+	response.send(str);
+});
+
+app.post("/register", function (request, response) {
+	// process a simple register form
+	let POST = request.body;
+	console.log(POST);
+	let user_name = POST["username"];
+	let user_pass = POST["password"];
+	let user_email = POST["email"];
+	let user_pass2 = POST["repeat_password"];
+
+	if (users[user_name] == undefined) {
+		users[user_name] = {};
+		users[user_name].name = user_name;
+		users[user_name].password = user_pass;
+		users[user_name].email = user_email;
+
+		let data = JSON.stringify(users);
+		fs.writeFileSync(fname, data, "utf-8");
+
+		response.send("Got a registration");
+	} else {
+		response.send("User " + user_name + " already exists!");
+	}
+});
 // start server
 app.listen(8080, () => console.log(`listening on port 8080`));
